a518ad85e9db47ae1ff2a8174158fdda805772ff
zerashih <zerashih@gmail.com>
2020-04-18 08:12:05 +0000

    Second Part of CA2

---
+++ mobdev-ca2/src/app/pages/episodes/episodes.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ApiService } from '../../services/api.service';
+@Component({
+  selector: 'app-episodes',
+  templateUrl: './episodes.page.html',
+  styleUrls: ['./episodes.page.scss'],
+})
+export class EpisodesPage implements OnInit {
+  episodes: Observable<any>;
+  constructor(private router: Router, private api: ApiService) { }
+  ngOnInit() {
+      this.episodes = this.api.getEpisodes();
+  }
+}
