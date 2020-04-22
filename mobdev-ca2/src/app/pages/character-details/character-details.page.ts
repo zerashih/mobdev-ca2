f3730ab54a08025f355896d435994313023fc5ae
zerashih <zerashih@gmail.com>
2020-04-22 11:12:11 +0000

    Like/ Dislike Buttons

--- mobdev-ca2/src/app/pages/character-details/character-details.page.ts
+++ mobdev-ca2/src/app/pages/character-details/character-details.page.ts
@@ -9,7 +9,19 @@ import { ActivatedRoute } from '@angular/router';
 export class CharacterDetailsPage implements OnInit {
   character: any;
   characterId = null;
+  LikeValue: number=0;
+  DislikeValue: number=0;
+
   constructor(private activatedRoute: ActivatedRoute, private api: ApiService) {}
+  
+  handleLike(){
+      this.LikeValue++;
+  }
+
+  handlDislike(){
+      this.DislikeValue++;
+  }
+
   ngOnInit() {
     this.characterId = this.activatedRoute.snapshot.paramMap.get('id');
     this.api.getCharacter(this.characterId).subscribe(res => {
