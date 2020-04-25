879b1ad02fcf7a795365c0f3e97432bdeb69cd1f
zerashih <zerashih@gmail.com>
2020-04-25 06:17:31 +0000

    loadCharacter()

--- mobdev-ca2/src/app/pages/characters/characters.page.ts
+++ mobdev-ca2/src/app/pages/characters/characters.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ApiService } from '../../services/api.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-characters',
@@ -10,7 +11,25 @@ import { ApiService } from '../../services/api.service';
 })
 export class CharactersPage implements OnInit {
     characters: Observable<any>;
-    constructor(private router: Router, private api: ApiService) { }
+
+    scrollChar = [];
+    offset = 0;
+   
+
+    constructor(private router: Router, private api: ApiService, private http: HttpClient) { 
+     this.loadCharacters();
+    }
+
+    loadCharacters(){
+        this.http.get(`https://www.breakingbadapi.com/api/characters?limit=10&offset=${this.offset}`)
+        .subscribe(res => {
+            console.log(res);
+            this.scrollChar = this.scrollChar.concat(['limit']);
+        })
+    }
+
+
+
     ngOnInit() {
         this.characters = this.api.getCharacters();
         this.characters.subscribe(data => {
